e572f8b1f29212a3d62652e74e525efa21836c68
Igna Luna <ignaluna98pb@gmail.com>
2023-05-22 12:32:24 -0300

    Auth Done. DB con mogonse

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,33 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next/auth/providers/google"
+import GoogleProvider from "next-auth/providers/google"
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
+
 
 const handler = NextAuth({
-    poviders: [
+    providers: [
         GoogleProvider({
-            clientId: "",
-            clientSecret: "",
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({session}){
+    async session({ session }) {
 
     },
-    async signIn({Profile}){
+    async signIn({ Profile }) {
+        try {
+            await connectToDB();
+            // User already exists?
+const userExist = await User.findOne({})
+            //Create user
+
+            return true;
+        } catch (error) {
+            console.log(error)
+        }
 
     },
 })
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
